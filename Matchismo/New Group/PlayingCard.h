f9781b471985b1855afce07a2c85e8e81ad9d2c8
Can <can@Cans-MacBook-Pro.local>
2018-07-30 11:15:50 +0300

    deck class defined , PlayingCard class created

---
+++ Matchismo/New Group/PlayingCard.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCard.h
+//  Matchismo
+//
+//  Created by Can on 30.07.2018.
+//  Copyright © 2018 Winlentia. All rights reserved.
+//
+
+#import "Card.h"
+
+@interface PlayingCard : Card
+
+@end
---
+++ Matchismo/New Group/PlayingCard.m
@@ -0,0 +1,13 @@
+//
+//  PlayingCard.m
+//  Matchismo
+//
+//  Created by Can on 30.07.2018.
+//  Copyright © 2018 Winlentia. All rights reserved.
+//
+
+#import "PlayingCard.h"
+
+@implementation PlayingCard
+
+@end
