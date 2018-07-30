decb3f294d225370a1711cd2f78ab2edc839b3b4
Can <can@Cans-MacBook-Pro.local>
2018-07-30 12:29:14 +0300

    PlayingCard generated

--- Matchismo/New Group/PlayingCard.h
+++ Matchismo/New Group/PlayingCard.h
@@ -10,4 +10,12 @@
 
 @interface PlayingCard : Card
 
+@property (strong, nonatomic) NSString *suit;
+@property (nonatomic) NSUInteger rank;
+
++ (NSArray *)validSuits;
+
++ (NSArray *)rankStrings;
++ (NSUInteger) maxRank;
+
 @end
